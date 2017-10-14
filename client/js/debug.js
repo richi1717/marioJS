131fe7a454586629d14ee50179c8f48dacb43c13
Richi Jeffery <rici1717@gmail.com>
2017-10-14 13:11:54 -0700

    add Camera and clean up

--- client/js/debug.js
+++ client/js/debug.js
@@ -6,8 +6,9 @@ export function setupMouseControl(canvas, entity, camera) {
       if (event.buttons === 1) {
         entity.vel.set(0, 0);
         entity.pos.set(
-        event.offsetX + camera.pos.x,
-        event.offsetY + camera.pos.y);
+          event.offsetX + camera.pos.x,
+          event.offsetY + camera.pos.y
+        );
       } else if (event.buttons === 2
         && lastEvent && lastEvent.buttons === 2
         && lastEvent.type === 'mousemove') {
--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,10 @@
+import Camera from './Camera';
 import Timer from './Timer';
 import { loadLevel } from './loaders';
 import { createMario } from './entities';
-import { createCollisionLayer } from './layers';
+import { createCollisionLayer, createCameraLayer } from './layers';
 import { setupKeyboard } from './input';
+import { setupMouseControl } from './debug';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -12,20 +14,27 @@ Promise.all([
   loadLevel('1-1')
 ])
   .then(([mario, level]) => {
+    const camera = new Camera();
+
     mario.pos.set(64, 64);
 
-    level.comp.layers.push(createCollisionLayer(level));
+    level.comp.layers.push(
+      createCollisionLayer(level),
+      createCameraLayer(camera)
+    );
 
     level.entities.add(mario);
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
 
+    setupMouseControl(canvas, mario, camera);
+
     const timer = new Timer(1 / 60);
 
     timer.update = function update(deltaTime) {
       level.update(deltaTime);
-      level.comp.draw(context);
+      level.comp.draw(context, camera);
     };
 
     timer.start();
